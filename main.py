cd434ed63948456211303c7d76a78bb8a0c8fedb
AryanS62 <58567476+AryanS62@users.noreply.github.com>
2020-04-30 16:53:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1060,9 +1060,10 @@ def co_order_list_c():
                 val=(c_id,)
                 cur.execute(cmd,val)
                 orders=cur.fetchall()
-	for i in orders:
-	    if i[6]=='Sent':
-			i[6]='Received'
+    for i in orders:
+        if i[6]=='Sent':
+            i[6]='Recieved'
+	
     return render_template('co_order_list_c.html',table=orders)
     
 if __name__ == '__main__':
