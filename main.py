f9e03a9ebce75e436ef171f6ac5c8b6dbf8d9c62
jagseer-singh <61871052+jagseer-singh@users.noreply.github.com>
2020-04-30 16:37:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1061,9 +1061,9 @@ def co_order_list_c():
                 cur.execute(cmd,val)
                 orders=cur.fetchall()
     for i in orders:
-	if i[6]=='Sent':
-		i[6]='Recieved'	
-                
+        if i[6]=='Sent':
+	    	i[6]='Recieved'	
+             
     return render_template('co_order_list_c.html',table=orders)
     
 if __name__ == '__main__':
